f5d5298c38f59f1762067ebf2488e506b99bb00c
Anwesh43 <anweshthecool0@gmail.com>
2021-03-20 01:32:50 +0530

    adding BiRectLineUp class

--- birectlineupview/src/main/java/com/example/birectlineupview/BiRectLineUpView.kt
+++ birectlineupview/src/main/java/com/example/birectlineupview/BiRectLineUpView.kt
@@ -163,4 +163,27 @@ class BiRectLineUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiRectLineUp(var i : Int) {
+
+        private var curr : BRLUNode = BRLUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
