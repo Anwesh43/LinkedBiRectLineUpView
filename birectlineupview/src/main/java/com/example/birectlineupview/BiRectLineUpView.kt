42d9048d0c6ba2e35fc80f8353e8bdd97bbdc935
Anwesh43 <anweshthecool0@gmail.com>
2021-03-20 01:14:39 +0530

    adding Canvas extension methods

--- birectlineupview/src/main/java/com/example/birectlineupview/BiRectLineUpView.kt
+++ birectlineupview/src/main/java/com/example/birectlineupview/BiRectLineUpView.kt
@@ -28,3 +28,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiRectLineUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawRect(RectF(-w /2, -h / 2, -w / 2 + w * 0.5f * sf1, 0f), paint)
+        drawLine(-w / 4, -h / 2, -w / 4, -h / 2 - h * 0.5f * sf2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBRLUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRectLineUp(scale, w, h, paint)
+}
+
